e9f0fc265a7a651c8891bffcd443b210cd87eb4d
dilar <dilar@LAPTOP-V6781U9D>
2023-01-26 08:31:40 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using notebookclass;
+using System;
+
+namespace notebookclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            notebook[] pc = new notebook[3];
+
+            for (int i = 0; i < pc.Length; i++)
+            {
+                Console.WriteLine("Brandi Daxil Edin");
+                string brand = Console.ReadLine();
+
+
+                Console.WriteLine("Modeli Daxil edin");
+                string model = Console.ReadLine();
+
+                double price;
+                do
+                {
+                    Console.WriteLine("Price Daxil Edin");
+                    string Price = Console.ReadLine();
+                    price = Convert.ToDouble(Price);
+
+                } while ( price <= 0);
+
+                Console.WriteLine("Model Daxil Edin");
+                string Model = Console.ReadLine();
+
+                notebook Pc = new notebook(brand,Model, price );
+                pc[i] = Pc;
+            }
+                   double sum = 0;
+            for (int i = 0; i < pc.Length; i++)
+            {
+                sum += pc[i].Price;
+                
+            }
+            double average = sum / pc.Length;
+            Console.WriteLine(average);
+        }  
+
+
+    }
+}
---
+++ notebook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace notebookclass
+{
+    internal class notebook
+    {
+        public string Brand;
+        public string Model;
+        public double Price;
+
+        public notebook(string brand, string model, double price = 0)
+        {
+            Brand = brand;
+            Model = model;
+            Price = price;
+
+        }
+        public  string Showinfo()
+        {
+            return $"Brand: {Brand},Model: {Model}, Price: {Price}";
+        }
+    }
+}
+
+
+
+
+
+
+
+
